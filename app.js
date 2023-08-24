61a47984b17204d4b1dd8f4e99dbf1d8c63a9e9b
Isaac Aftergut <ijagut@vt.edu>
2023-08-24 13:56:36 -0400

    following along

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const container = document.querySelector('.container');
-const colors = ['red', 'blue', 'green'];
-const sizes = ['sm', 'md', 'lg'];
+const colors = ['red', 'blue', 'green', 'yellow'];
+const sizes = ['sm', 'md', 'lg', 'xl'];
+const type = ['circle', 'square'];
 
 function randomShape(){
   const colorIdx = Math.floor(Math.random()*colors.length);
   const sizeIdx = Math.floor(Math.random()*sizes.length);
+  const typeIx = Math.floor(Math.random()*type.length);
   const shape = {
     color: colors[colorIdx],
-    size: sizes[sizeIdx]
+    size: sizes[sizeIdx],
+    type: type[typeIx]
 
   };
   return shape;
@@ -19,15 +22,18 @@ const shapes = [
 ];
 
 function render(){
-  const html = shapes.map(function(shape){
-    return `<div class='${shape.color} ${shape.size}'></div>`;
+  const html = shapes.map(function(shape, idx){
+    return `<div class='${shape.color} ${shape.size} ${shape.type}'>${idx+1}</div>`;
   });
   container.innerHTML = html.join('');
 }
 
 render();
-setInterval(function(){
+const i = setInterval(function(){
   const newShape = randomShape();
   shapes.push(newShape);
   render();
+  if (shapes.length === 20){
+    clearInterval(i)
+  }
 }, 500);
